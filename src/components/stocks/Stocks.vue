3453dee84ae846f7ec8a5d45c5f497495ec4528e
Stephen Axtell <axtell.stephen@gmail.com>
2019-06-26 13:12:36 +0200

    defing the stocks and stock layouts

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Stocks Component</h1>
+  <div>
+    <app-stock v-for="stock in stocks" :key="stock.id"></app-stock>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue';
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          {id: 1, name: 'Mercedes', price: 200 },
+          {id: 2, name: 'Sasol', price: 100 },
+          {id: 1, name: 'Coca Cola', price: 300 },
+          {id: 1, name: 'Simba', price: 50 }
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
