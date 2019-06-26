2f800316183a0b94c5a29ecd9bb70e1465638f4a
Stephen Axtell <axtell.stephen@gmail.com>
2019-06-26 16:19:20 +0200

    adding Vuex state and creating all the getters, actions and mutations

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,18 +8,13 @@
   import Stock from './Stock.vue';
 
   export default {
-    data() {
-      return {
-        stocks: [
-          {id: 1, name: 'Mercedes', price: 200 },
-          {id: 2, name: 'Sasol', price: 100 },
-          {id: 3, name: 'Coca Cola', price: 300 },
-          {id: 4, name: 'Simba', price: 50 }
-        ]
-      }
-    },
     components: {
       appStock: Stock
+    },
+    computed: {
+      stocks() {
+        return this.$store.getters.stocks;
+      }
     }
   }
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  {id: 1, name: 'Mercedes', price: 200 },
+  {id: 2, name: 'Sasol', price: 100 },
+  {id: 3, name: 'Coca Cola', price: 300 },
+  {id: 4, name: 'Simba', price: 50 }
+];
